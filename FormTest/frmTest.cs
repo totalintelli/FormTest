03104baf6c349390bfa2627c96989c2057c4db36
totalintelli <kwanwoo5@naver.com>
2015-12-13 21:53:15 +0900

    frmTest 폼의 폼 생성 기능 추가

--- FormTest/frmTest.cs
+++ FormTest/frmTest.cs
@@ -23,7 +23,7 @@ namespace FormTest
             cmbType.Items.Add("None");
             cmbType.Items.Add("FixedSingle");
             cmbType.Items.Add("Fixed3D");
-            cmbType.Items.Add("FoxedDialog");
+            cmbType.Items.Add("FixedDialog");
             cmbType.Items.Add("Sizable");
             cmbType.Items.Add("FixedToolWindow");
             cmbType.Items.Add("SizableToolWindow");
@@ -45,7 +45,48 @@ namespace FormTest
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
+            FormTest.frmCreate testDialog = new FormTest.frmCreate();
 
+            // 속성 설정
+            if (cmbType.SelectedIndex == 0)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            else if (cmbType.SelectedIndex == 1)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            else if (cmbType.SelectedIndex == 2)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            else if (cmbType.SelectedIndex == 3)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            else if (cmbType.SelectedIndex == 4)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            else if (cmbType.SelectedIndex == 5)
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            else
+                testDialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+
+            if (cmbPos.SelectedIndex == 0)
+                testDialog.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            else if (cmbPos.SelectedIndex == 1)
+                testDialog.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            else if (cmbPos.SelectedIndex == 2)
+                testDialog.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            else if (cmbPos.SelectedIndex == 3)
+                testDialog.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            else
+                testDialog.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+
+            // 속성에 맞는 문자열 출력
+            testDialog.lblType.Text = cmbType.SelectedItem.ToString();
+            testDialog.lblPos.Text = cmbType.SelectedItem.ToString();
+
+            if (cmbMode.SelectedIndex == 0)
+            {
+                testDialog.ShowDialog(this);
+                testDialog.Dispose();
+            }
+            else
+            {
+                testDialog.Show();
+            }
         }
     }
 }
