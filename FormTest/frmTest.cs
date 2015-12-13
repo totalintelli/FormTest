9237eb9a7067ff27aab1809ed2fa47fa01fcff41
totalintelli <kwanwoo5@naver.com>
2015-12-13 21:28:46 +0900

    frmTest 폼에 콤보 상자 추가

--- FormTest/frmTest.cs
+++ FormTest/frmTest.cs
@@ -16,5 +16,31 @@ namespace FormTest
         {
             InitializeComponent();
         }
+
+        private void frmTest_Load(object sender, EventArgs e)
+        {
+            // 콤보 상자 초기값 설정
+            cmbType.Items.Add("None");
+            cmbType.Items.Add("FixedSingle");
+            cmbType.Items.Add("Fixed3D");
+            cmbType.Items.Add("FoxedDialog");
+            cmbType.Items.Add("Sizable");
+            cmbType.Items.Add("FixedToolWindow");
+            cmbType.Items.Add("SizableToolWindow");
+
+            cmbPos.Items.Add("Manual");
+            cmbPos.Items.Add("CenterScreen");
+            cmbPos.Items.Add("WindowsDafaultLocation");
+            cmbPos.Items.Add("WindowsDefaultBounds");
+            cmbPos.Items.Add("CenterParent");
+
+            cmbMode.Items.Add("모달");
+            cmbMode.Items.Add("모달리스");
+
+            // 초기 선택값 설정
+            cmbType.SelectedIndex = 0;
+            cmbPos.SelectedIndex = 0;
+            cmbMode.SelectedIndex = 0;
+        }
     }
 }
